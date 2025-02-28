faca0f6e4040b4a0f947b0247371620d777aab7e
jels7 <cucchiaa@oregonstate.edu>
2025-02-27 19:22:28 -0800

    Updated app.js

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ var exphbs = require('express-handlebars');     // Import express-handlebars
 app.engine('.hbs', engine({ extname: ".hbs" }));  // Create an instance of the handlebars engine to process templates
 app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 
+
+
 // Render the homepage
 app.get('/', function (req, res) {
     res.render('index');
@@ -31,6 +33,14 @@ app.get('/patrons', function (req, res) {
     let query1 = "SELECT * FROM Patrons;";
     db.pool.query(query1, function (error, rows, fields) {
         res.render('patrons', { data: rows });
+    });
+});
+
+// Render the books page
+app.get('/books', function (req, res) {
+    let query1 = "SELECT * FROM Books;";
+    db.pool.query(query1, function (error, rows, fields) {
+        res.render('books', { data: rows });
     });
 });
 
@@ -87,6 +97,57 @@ app.post('/add-patron-form', function (req, res) {
     });
 });
 
+// Add a book via AJAX
+app.post('/add-book-ajax', function (req, res) {
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Create the query and run it on the database
+    let query1 = `INSERT INTO Books (bookTitle, genreID, copiesAvailable) VALUES ('${data.bookTitle}', '${data.genreID}', '${data.copiesAvailable}')`;
+    db.pool.query(query1, function (error, rows, fields) {
+        // Check to see if there was an error
+        if (error) {
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error);
+            res.sendStatus(400);
+        } else {
+            // If there was no error, perform a SELECT * on Books
+            let query2 = `SELECT * FROM Books`;
+            db.pool.query(query2, function (error, rows, fields) {
+                // If there was an error on the second query, send a 400
+                if (error) {
+                    // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                    console.log(error);
+                    res.sendStatus(400);
+                } else {
+                    // If all went well, send the results of the query back.
+                    res.send(rows);
+                }
+            });
+        }
+    });
+});
+
+// Add a book via form submission
+app.post('/add-book-form', function (req, res) {
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Create the query and run it on the database
+    let query1 = `INSERT INTO Books (bookTitle, genreID, copiesAvailable) VALUES ('${data['input-bookTitle']}', '${data['input-genreID']}', '${data['input-copiesAvailable']}')`;
+    db.pool.query(query1, function (error, rows, fields) {
+        // Check to see if there was an error
+        if (error) {
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error);
+            res.sendStatus(400);
+        } else {
+            // If there was no error, redirect back to the books page
+            res.redirect('/books');
+        }
+    });
+});
+
 // DELETE ROUTE
 
 // Delete a patron via AJAX
@@ -97,6 +158,23 @@ app.delete('/delete-patron-ajax', function (req, res) {
 
     // Run the delete query
     db.pool.query(deletePatronQuery, [patronID], function (error, rows, fields) {
+        if (error) {
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error);
+            res.sendStatus(400);
+        } else {
+            res.sendStatus(204);
+        }
+    });
+});
+// Delete a book via AJAX
+app.delete('/delete-book-ajax', function (req, res) {
+    let data = req.body;
+    let bookID = parseInt(data.id);
+    let deleteBookQuery = `DELETE FROM Books WHERE bookID = ?`;
+
+    // Run the delete query
+    db.pool.query(deleteBookQuery, [bookID], function (error, rows, fields) {
         if (error) {
             // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
             console.log(error);
@@ -129,6 +207,38 @@ app.put('/update-patron-ajax', function (req, res) {
         } else {
             // Run the select query to get the updated data
             db.pool.query(selectUpdatedPatron, [patronID], function (error, rows, fields) {
+                if (error) {
+                    console.log(error);
+                    res.sendStatus(400);
+                } else {
+                    res.send(rows);
+                }
+            });
+        }
+    });
+});
+
+// Update a book via AJAX
+app.put('/update-book-ajax', function (req, res) {
+    let data = req.body;
+
+    let bookID = parseInt(data.bookID);
+    let bookTitle = data.bookTitle;
+    let genreID = data.genreID;
+    let copiesAvailable = data.copiesAvailable;
+
+    let queryUpdateBook = `UPDATE Books SET bookTitle = ?, genreID = ?, copiesAvailable = ? WHERE bookID = ?`;
+    let selectUpdatedBook = `SELECT * FROM Books WHERE bookID = ?`;
+
+    // Run the update query
+    db.pool.query(queryUpdateBook, [bookTitle, genreID, copiesAvailable, bookID], function (error, rows, fields) {
+        if (error) {
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error);
+            res.sendStatus(400);
+        } else {
+            // Run the select query to get the updated data
+            db.pool.query(selectUpdatedBook, [bookID], function (error, rows, fields) {
                 if (error) {
                     console.log(error);
                     res.sendStatus(400);

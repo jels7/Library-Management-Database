22b2ac13be292fa143ad9448b772878b4f612c2b
Anjelica <146127905+jels7@users.noreply.github.com>
2025-03-12 11:06:38 -0700

    Added citation

--- public_html/js/patrons.js
+++ public_html/js/patrons.js
@@ -1,3 +1,11 @@
+// Citation
+// Based on the CS 340 Node.js Starter Guide
+// Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app
+// 3/12/2025
+// Group 7
+// Meredith Baker & Anjelica Cucchiara
+
+
 // Get the objects we need to modify
 let addPatronForm = document.getElementById('add-patron-form-ajax');
 

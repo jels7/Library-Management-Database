14fde765cacacc7d8f15b611c21092c70a443a32
Anjelica <146127905+jels7@users.noreply.github.com>
2025-03-12 11:05:22 -0700

    Added citation

--- public_html/js/delete_patrons.js
+++ public_html/js/delete_patrons.js
@@ -1,3 +1,12 @@
+// Citation
+// Based on the CS 340 Node.js Starter Guide
+// Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app
+// 3/12/2025
+// Group 7
+// Meredith Baker & Anjelica Cucchiara
+
+
+
 function deletePatron(patronID) {
     // Put our data we want to send in a javascript object
     let data = {

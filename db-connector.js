591df5da8328b02900ceb59dc4b2a762597d6cd9
Anjelica <146127905+jels7@users.noreply.github.com>
2025-03-12 10:50:28 -0700

    Added citation

--- db-connector.js
+++ db-connector.js
@@ -1,3 +1,11 @@
+// Citation
+// Based on the CS 340 Node.js Starter Guide
+// Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app
+// 3/12/2025
+// Group 7
+// Meredith Baker & Anjelica Cucchiara
+
+
 // Get an instance of mysql we can use in the app
 var mysql = require('mysql');
 

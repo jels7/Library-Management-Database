9b5b3c337fdfb55b17f45c06fd7ea78f55bec51e
Anjelica <146127905+jels7@users.noreply.github.com>
2025-03-12 11:06:54 -0700

    Added citation

--- public_html/js/update_book.js
+++ public_html/js/update_book.js
@@ -1,3 +1,12 @@
+// Citation
+// Based on the CS 340 Node.js Starter Guide
+// Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app
+// 3/12/2025
+// Group 7
+// Meredith Baker & Anjelica Cucchiara
+
+
+
 // Get the objects we need to modify
 let updateBookForm = document.getElementById('update-book-form-ajax');
 
